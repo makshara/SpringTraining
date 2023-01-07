66c67aa6cace9eaffc62b8bce0b9ce2a83ea051c
makshara <aksharam0697@gmail.com>
2023-01-07 14:39:08 -0500

    Initial commit

---
+++ src/main/java/com/spring/training/MyController.java
@@ -0,0 +1,70 @@
+package com.spring.training;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.training.models.UserHeaders;
+/**
+ * 
+ * @author banda
+ *
+ */
+@RestController
+@RequestMapping(value="/Get")
+public class MyController {
+	
+
+	/**
+	 * 
+	 * @return
+	 */
+    @RequestMapping(value = "/getName",method = RequestMethod.GET)
+    public String getName() {
+    	String name = "yash";
+    	return name;
+        
+    }
+    
+   // @RequestMapping(value = "/getFirstName", method = RequestMethod.GET)
+    @GetMapping(value = "/getFirstName")
+    public String getFirstName() {
+    	return "firstName";
+    }
+    /**
+     * 
+     * @param personName
+     * @return
+     */
+    @RequestMapping(value = "/getNameWithParameters",method = RequestMethod.GET)
+    public String getNameWithParameters(@RequestParam(defaultValue = "orange") String personName) {
+    	System.out.println(personName);
+    	return personName;
+        
+    }
+    
+    @RequestMapping(produces = "application/json",value = "/getHttpHeaders",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<UserHeaders> getHttpHeaders(@RequestHeader String userAgent,
+    		@RequestHeader String accept,
+    		@RequestHeader( name = "accept-encoding") String acceptEncoding,
+    		@RequestHeader( name = "content-type") String contentType ,
+    		@RequestParam(defaultValue = "banana") String data) {
+    	UserHeaders uHeader = new UserHeaders();
+    	uHeader.setAccept(accept);
+    	uHeader.setAcceptEncoding(acceptEncoding);
+    	uHeader.setContentType(contentType);
+    	
+    	System.out.println(userAgent);
+    	System.out.println(data);
+    	return new ResponseEntity<UserHeaders>(uHeader,HttpStatus.OK);
+        
+    }
+}
---
+++ src/main/java/com/spring/training/models/UserHeaders.java
@@ -0,0 +1,32 @@
+package com.spring.training.models;
+
+public class UserHeaders {
+	private String acceptEncoding, contentType, accept;
+
+	public String getAcceptEncoding() {
+		return acceptEncoding;
+	}
+
+	public void setAcceptEncoding(String acceptEncoding) {
+		this.acceptEncoding = acceptEncoding;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public String getAccept() {
+		return accept;
+	}
+
+	public void setAccept(String accept) {
+		this.accept = accept;
+	} 
+	
+	
+
+}
